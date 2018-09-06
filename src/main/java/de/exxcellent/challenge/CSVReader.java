c3c3c214b4697388ad9b054ef363071e3786e241
Ziyi Bao <ziyi.bao14@imperial.ac.uk>
2018-09-06 16:26:26 +0200

    Add CSVReader skeleton and unit tests for CSVReader

---
+++ src/main/java/de/exxcellent/challenge/CSVReader.java
@@ -0,0 +1,11 @@
+package de.exxcellent.challenge;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.IOException;
+
+public class CSVReader {
+	public static ArrayList<HashMap<String, String>> parseIntoArrayList(String fileName) throws IOException{
+		return null;
+	}
+}
---
+++ src/test/java/de/exxcellent/challenge/CSVReaderTest.java
@@ -0,0 +1,55 @@
+package de.exxcellent.challenge;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.IOException;
+
+public class CSVReaderTest {
+
+	private final static String FILE_PREFIX = "/de/exxcellent/challenge/";
+	private final static String EMPTY_CSV_FILE = FILE_PREFIX + "testDataEmpty.csv";
+	private final static String SIMPLE_CSV_FILE = FILE_PREFIX + "testDataSimple.csv";
+	private final static String CSV_FILE_WITH_SPACES = FILE_PREFIX + "testDataWithSpaces.csv";
+	private final static String MALFORMED_CSV_FILE = FILE_PREFIX + "testDataMalformed.csv";
+	
+	@Test
+	public void simpleCSVFileShouldBeParsedAsEmptyArrayList() {
+		URL url = getClass().getResource(EMPTY_CSV_FILE);
+		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+		Assert.assertTrue(dataArray.isEmpty());
+	}
+	
+	@Test
+	public void simpleCSVFileShouldBeParsed() {
+		final String HEADER_1 = "Header1";
+		final String HEADER_2 = "Header2";
+		URL url = getClass().getResource(SIMPLE_CSV_FILE);
+		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+		Assert.assertEquals("1", dataArray.get(0).get(HEADER_1));
+		Assert.assertEquals("2", dataArray.get(0).get(HEADER_1));
+		Assert.assertEquals("3", dataArray.get(1).get(HEADER_2));
+		Assert.assertEquals("4", dataArray.get(1).get(HEADER_2));
+	}
+	
+	@Test
+	public void CSVFileWithSpacesShouldBeParsed() {
+		final String HEADER_1 = "Header 1";
+		final String HEADER_2 = "Header 2";
+		URL url = getClass().getResource(CSV_FILE_WITH_SPACES);
+		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+		Assert.assertEquals("1", dataArray.get(0).get(HEADER_1));
+		Assert.assertEquals("2", dataArray.get(0).get(HEADER_1));
+		Assert.assertEquals("3", dataArray.get(1).get(HEADER_2));
+		Assert.assertEquals("4", dataArray.get(1).get(HEADER_2));
+	}
+	
+	@Test(expected = IOException.class)
+	public void MalformedCSVFileShouldThrowException() {
+		URL url = getClass().getResource(MALFORMED_CSV_FILE);
+		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+	}
+}
