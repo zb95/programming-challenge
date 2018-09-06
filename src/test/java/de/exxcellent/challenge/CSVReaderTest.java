b40654f25957bcc2813e4ed911b152ad79fe1114
Ziyi Bao <ziyi.bao14@imperial.ac.uk>
2018-09-06 21:16:18 +0200

    Fix bugs in unit tests for CSVReader

--- src/test/java/de/exxcellent/challenge/CSVReaderTest.java
+++ src/test/java/de/exxcellent/challenge/CSVReaderTest.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 
 public class CSVReaderTest {
 
+	/* private final static String FILE_PREFIX = "/de/exxcellent/challenge/"; */
 	private final static String FILE_PREFIX = "/de/exxcellent/challenge/";
 	private final static String EMPTY_CSV_FILE = FILE_PREFIX + "testDataEmpty.csv";
 	private final static String SIMPLE_CSV_FILE = FILE_PREFIX + "testDataSimple.csv";
@@ -17,39 +18,45 @@ public class CSVReaderTest {
 	private final static String MALFORMED_CSV_FILE = FILE_PREFIX + "testDataMalformed.csv";
 	
 	@Test
-	public void simpleCSVFileShouldBeParsedAsEmptyArrayList() {
+	public void emptyCSVFileShouldBeParsedAsEmptyArrayList() throws IOException{
 		URL url = getClass().getResource(EMPTY_CSV_FILE);
-		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+		ArrayList<HashMap<String, String>> dataArray = CSVReader.parseIntoArrayList(url.getPath());
 		Assert.assertTrue(dataArray.isEmpty());
 	}
 	
 	@Test
-	public void simpleCSVFileShouldBeParsed() {
+	public void simpleCSVFileShouldBeParsed() throws IOException{
 		final String HEADER_1 = "Header1";
 		final String HEADER_2 = "Header2";
 		URL url = getClass().getResource(SIMPLE_CSV_FILE);
-		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+		ArrayList<HashMap<String, String>> dataArray = CSVReader.parseIntoArrayList(url.getPath());
 		Assert.assertEquals("1", dataArray.get(0).get(HEADER_1));
-		Assert.assertEquals("2", dataArray.get(0).get(HEADER_1));
-		Assert.assertEquals("3", dataArray.get(1).get(HEADER_2));
+		Assert.assertEquals("2", dataArray.get(0).get(HEADER_2));
+		Assert.assertEquals("3", dataArray.get(1).get(HEADER_1));
 		Assert.assertEquals("4", dataArray.get(1).get(HEADER_2));
 	}
 	
 	@Test
-	public void CSVFileWithSpacesShouldBeParsed() {
+	public void CSVFileWithSpacesShouldBeParsed() throws IOException{
 		final String HEADER_1 = "Header 1";
 		final String HEADER_2 = "Header 2";
 		URL url = getClass().getResource(CSV_FILE_WITH_SPACES);
-		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+		ArrayList<HashMap<String, String>> dataArray = CSVReader.parseIntoArrayList(url.getPath());
 		Assert.assertEquals("1", dataArray.get(0).get(HEADER_1));
-		Assert.assertEquals("2", dataArray.get(0).get(HEADER_1));
-		Assert.assertEquals("3", dataArray.get(1).get(HEADER_2));
+		Assert.assertEquals("2", dataArray.get(0).get(HEADER_2));
+		Assert.assertEquals("3", dataArray.get(1).get(HEADER_1));
 		Assert.assertEquals("4", dataArray.get(1).get(HEADER_2));
 	}
 	
-	@Test(expected = IOException.class)
-	public void MalformedCSVFileShouldThrowException() {
-		URL url = getClass().getResource(MALFORMED_CSV_FILE);
-		ArrayList dataArray = CSVReader.parseIntoArrayList(url.getPath());
+	@Test
+	public void MalformedCSVFileShouldThrowException() throws IOException{
+		try {
+			URL url = getClass().getResource(MALFORMED_CSV_FILE);
+			ArrayList<HashMap<String, String>> dataArray = CSVReader.parseIntoArrayList(url.getPath());
+			Assert.fail("Expected an IOException to be thrown");
+		} catch (IOException ioEx) {
+			Assert.assertEquals("CSV entry must have as many columns as header!", ioEx.getMessage());
+		}
+		
 	}
 }
