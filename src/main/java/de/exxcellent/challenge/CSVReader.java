90dfba0ca9191ccd0062cec4a56479e7950ef9a0
Ziyi Bao <ziyi.bao14@imperial.ac.uk>
2018-09-06 20:06:43 +0200

    Implement CSVReader

--- src/main/java/de/exxcellent/challenge/CSVReader.java
+++ src/main/java/de/exxcellent/challenge/CSVReader.java
@@ -2,10 +2,37 @@ package de.exxcellent.challenge;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.io.FileReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 
+//TODO: change hashmap to map in declaration
 public class CSVReader {
 	public static ArrayList<HashMap<String, String>> parseIntoArrayList(String fileName) throws IOException{
-		return null;
+		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+			ArrayList<HashMap<String, String>> arr = 
+					new ArrayList<HashMap<String, String>>();
+			String line;
+			if ((line = br.readLine()) != null) {
+				String[] headers = line.split(",");
+				for (int i = 0; i < headers.length; i++) {
+					headers[i] = headers[i].trim();
+				}
+				
+				while ((line = br.readLine()) != null) {
+					String[] values = line.split(",");
+					if (headers.length != values.length) {
+						throw new IOException("CSV entry must have as many columns as header!");
+					}
+					
+					HashMap<String, String> map = new HashMap<String, String>();
+					for (int i = 0; i < values.length; i++) {
+						map.put(headers[i], values[i].trim());
+					}
+					arr.add(map);
+				}
+			}
+			return arr;
+		}
 	}
 }
